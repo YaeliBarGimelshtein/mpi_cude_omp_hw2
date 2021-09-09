a03a572e1eb844286192e0148759f135ab12ce86
YaeliBarGimelshtein <yaeli47@gmail.com>
2021-09-09 13:57:47 +0300

    STARTED STRUCT

--- histogram.c
+++ histogram.c
@@ -2,25 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <omp.h>
 
 #define ROOT 0
 
+typedef struct hist
+{
+    int number;
+    int count_of_number;
+} Hist;
 
+void create_hist_type(MPI_Datatype* hist_type)
+{
+    MPI_Type_contiguous(2, MPI_INT, hist_type);
+    MPI_Type_commit(hist_type);
+}
 
 int main(int argc, char *argv[])
 {
     //GENERAL INTEGERS NEEDED
     int my_rank, num_procs, input_size;
     int* input;
+    MPI_Datatype hist_type;
+    Hist* histogram;
     
     //MPI INIT
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs); 
 
-    //GET THE INPUT
     if(my_rank == ROOT)
     {
+        //GET THE INPUT
         scanf("%d",&input_size);
         getchar();
     
@@ -29,6 +42,17 @@ int main(int argc, char *argv[])
 
         for (int i = 0; i < input_size; i++)
            scanf("%d",&input[i]); 
+        
+        //CREATE THE HISTOGRAM
+        int different_nums = 0;
+#pragma omp parallel for 
+        for (int i = 0; i < input_size; i++)
+        {
+            
+        }
+        create_hist_type(&hist_type);
+        histogram = (Hist*)malloc(sizeof(Hist)*input_size); 
+
     }
      return 0;
 }
