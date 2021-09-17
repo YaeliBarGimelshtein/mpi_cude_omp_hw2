832ae8946738f626e25b212daeb119c9f11807f8
YaeliBarGimelshtein <yaeli47@gmail.com>
2021-09-17 17:22:26 +0300

    MORE CUDA

--- cuda.cu
+++ cuda.cu
@@ -7,17 +7,17 @@
 __global__ void calculateHistByCuda(int* input, int* histogram)
 {
     //CREATE HIST FOR EACH BLOCK
-    __shared__ int private_hist[SIZE] = {0};
+    //__shared__ int private_hist[SIZE] = {0};
 
     //CREATE INDEX FOR EACH THREAD IN EACH BLOCK
     int index = threadIdx.x + blockIdx.x * blockDim.x ; 
-
+    
     //COMPUTE HIST FOR EACH BLOCK
-    atomicAdd(&private_hist[input[index]], 1);
+    //atomicAdd(&private_hist[input[index]], 1);
     //private_hist[input[index]]++;
     
     //MERGE ALL PRIVATE HISTS INTO OUTPUT
-    atomicAdd(&histogram[index], 1);
+    atomicAdd(&histogram[input[index]], 1);
     //output[index] += private_hist[index];
 }
 
@@ -30,6 +30,22 @@ int* calculateHistByCuda(int* input, int size_of_input, int* result)
     if(size_of_input % NUM_THREADS_PER_BLOCK != 0)
         num_blocks++;
 
-    calculateHistByCuda<<<num_blocks, NUM_THREADS_PER_BLOCK>>>(input, result);
+    //ALLOCATE DATA TO CUDA MEMORY
+    int* cuda_input, *cuda_hist;
+    cudaMalloc((void**)&cuda_input, size_of_input);
+    cudaMalloc((void**)&cuda_hist, SIZE);
+    
+    //COPY INPUT INTO DEVICE
+    cudaMemcpy(cuda_input, input, size_of_input, cudaMemcpyHostToDevice);
+    
+    //LUNCH KERNEL
+    calculateHistByCuda<<<num_blocks, NUM_THREADS_PER_BLOCK>>>(cuda_input, cuda_hist);
+
+    //COPY RESULT BACK TO HOST
+    cudaMemcpy(result, cuda_hist, SIZE, cudaMemcpyDeviceToHost);
+
+    //FREE
+    cudaFree(cuda_input);
+    cudaFree(cuda_hist);
     return result;
 }
